1a6fa9d253e63fd7a76309288bdf3ef66bb8fc17
cmswormslev <cmswormslev@gmail.com>
2013-04-16 12:59:18 +0200

    Update html + add css to create user

--- createuser.php
+++ createuser.php
@@ -5,54 +5,22 @@ require_once "functions.php";
 <head>
     <title>Crtl-All-Shift</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="css/create_user.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-<table width="300" border="0" >
-    <form id="form1" name="form1" method="post" action="createEmp.php">
-        <tr>
-            <td colspan="3"><b>Create employee:</b></td>
-        </tr>
-        <tr>
-            <td>First name:</td>
-            <td>
-                <input type="text" name="txtFirstName"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Last name:</td>
-            <td>
-                <input type="text" name="txtLastName"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Address:</td>
-            <td>
-                <input type="text" name="txtAddress"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Zip code:</td>
-            <td>
-                <input type="text" name="txtZip"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Email:</td>
-            <td>
-                <input type="text" name="txtEmail"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Phone:</td>
-            <td>
-                <input type="text" name="txtPhone"/>
-            </td>
-        </tr>
-        <tr>
-            <td>Work function 1:</td>
-            <td>
-                <select name="selectWorkFunction1">
+<div id="createuser">
+	<table width="300">
+    	<form id="form1" name="form1" method="post" action="createEmp.php">
+	    	<fieldset>
+       			<legend>Create employee</legend>
+       				<label>First name:</label><input type="text" name="txtFirstName"/>
+       				<label>Last name:</label><input type="text" name="txtLastName"/>
+       				<label>Address:</label><input type="text" name="txtAddress"/>
+       				<label>Zip code:</label><input type="text" name="txtZip"/>
+       				<label>Email:</label><input type="text" name="txtEmail"/>
+	       			<label>Phone:</label><input type="text" name="txtPhone"/>
+	       			<label>Work function 1:</label><select name="selectWorkFunction1">
                     <?php
                     $sql_query = "SELECT skill_name, skill_id FROM skill";
                     $query_result = executeQuery($sql_query);
@@ -61,13 +29,8 @@ require_once "functions.php";
                         echo "<option value=\"" . $row['skill_id'] . "\">" . $skill ."</option>";
                     }
                     ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Work function 2:</td>
-            <td>
-                <select name="selectWorkFunction2">
+                    </select>
+                	<label>Work function 2:</label><select name="selectWorkFunction2">
                     <option value="-1"> None </option>
                     <?php
                     $sql_query = "SELECT skill_name, skill_id FROM skill";
@@ -78,13 +41,8 @@ require_once "functions.php";
                         echo "<option value=\"" . $row['skill_id'] . "\">" . $skill ."</option>";
                     }
                     ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-         <td>Work function 3:</td>
-            <td>
-                <select name="selectWorkFunction3">
+                    </select>
+                	<label>Work function 3:</label><select name="selectWorkFunction3">
                     <option value="-1"> None </option>
                     <?php
                     $sql_query = "SELECT skill_name, skill_id FROM skill";
@@ -94,21 +52,20 @@ require_once "functions.php";
                         echo "<option value=\"" . $row['skill_id'] . "\">" . $skill ."</option>";
                     }
                     ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <input type="submit" name="createUser" id="createUser" value="Create" />
-            </td>
-        </tr>
+                    </select>
+                    <input type="submit" name="createUser" id="createUser" value="Create" />
+	    	</fieldset>
 
-    </form>
-    <form id="deleteForm" name="delForm" method="post" action="delEmp.php">
-        <td>Select employee:</td>
-        <td>
-            <select name="selectEmpName">
-                <option value="-1"> None </option>
+	    </form>
+    </div>
+  
+    <div id="deleteuser">  
+	    <fieldset>
+		    <legend>Delete employee</legend>
+		    	<form id="deleteForm" name="delForm" method="post" action="delEmp.php">
+			    	<label>Select employee:</label>
+			    	<select name="selectEmpName">
+				    <option value="-1"> None </option>
                 <?php
                 $sql_query = "SELECT emp_id, CONCAT(first_name, ' ', last_name) as full_name FROM emp";
                 $query_result = executeQuery($sql_query);
@@ -118,15 +75,11 @@ require_once "functions.php";
                     echo "<option value=\"" . $row['emp_id'] . "\">" . $name ."</option>";
                 }
                 ?>
-            </select>
-        </td>
-        <tr>
-            <td colspan="2">
-                <input type="submit" name="delEmp" id="delEmp" value="Delete Employee" />
-            </td>
-        </tr>
-    </form>
+                	</select>
+                	<input type="submit" name="delEmp" id="delEmp" value="Delete Employee" />
+  
+        </form>
 
-</table>
+   </div>
 </body>
 </html>
