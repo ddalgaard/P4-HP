df67c6151aefee721addcb1c1642d8f459ae8ea2
Dennis Dalgaard <ddalgaard@gmail.com>
2013-05-24 17:47:35 +0200

    renamed main to dashboard

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <!-- Menu. If session is = 1 (meaning that the user logged in is an admin,
 the user is able to see the buttons linking to calendar.php and createuser.php) -->
 <ul id="menu">
-				<li><a href="main.php">Main</a></li>
+				<li><a href="main.php">Dashboard</a></li>
 				<?php if($_SESSION['isadmin'] == 1){ ?>
 					<li><a href="calendar.php">Calendar</a></li>
 					<li><a href="createuser.php">User management</a></li>
